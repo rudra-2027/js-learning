96ebe93d2d87f2061ae63dd983998a11b2638efa
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-06-01 17:30:09 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,3 +18,39 @@ console.log(c); // var is in if delecration but outside loop it is printing with
 // for(let i = 0;i<Array.length;i++){
 //     const elemnet = Array[i];
 // }
+//-------------------------------------------------------------
+function one(){
+    const username = "Rudra"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    two();
+    // console.log(website); // scope is inside website 
+}
+
+one();
+
+if(true){
+    const username = "Rudra"
+    if(username === "Rudra"){
+        const website = " youtube"
+        console.log(username+website)
+    }
+    // console.log(website) // ni de skta access
+}
+// console.log(username) // ni de skta access
+
+//+++++++++++++++++++++++++++++++++intersetinng ++++++++++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num){
+    return num+1;
+}
+// console.log(addTwo(5)); // this case is not possible 
+//Hosting
+const addTwo = function(num){ /* this is expression*/
+    return num+2;
+}
+addTwo(5)
