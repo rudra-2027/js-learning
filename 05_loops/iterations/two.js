e9929e9517ee5c877ae7dd8c2d6708f4e09364f7
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-06-02 08:57:18 +0000

    whileLoop

---
+++ 05_loops/iterations/two.js
@@ -0,0 +1,26 @@
+// while
+
+// while (condition) {
+    
+// }
+let idx = 0;
+while (idx<=10) {
+    console.log(`Value of idex is ${idx}`);
+    idx++;
+    
+}
+let myArray = ['flash','batman','superman']
+let arr = 0;
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+// do{
+
+// }while();
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
